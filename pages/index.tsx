163cd3fd8e784f44ee4e4cee67138df23f6123f5
João Rambo <jpramboguanabara@hotmail.com>
2022-08-25 12:37:56 -0300

    fixing overscroll bug and changing text center on mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,16 +17,18 @@ export default function Home() {
 
   if (!address) {
     return (
-      <div className="w-screen overscroll-x-none h-screen bg-gradient-to-b from-purple300 to-gray900 px-12">
+      <div className="w-screen h-full bg-gradient-to-b from-purple300 to-gray900 px-12">
         <div className="max-w-[1060px] mx-auto flex justify-between items-center pt-24">
-          <div className="max-w-[420px] leading-none space-y-8">
+          <div className="max-w-[420px] leading-none space-y-8 mx-auto lg:mx-0 text-center lg:text-left">
             <h1 className="text-white font-bold text-[60px]">
               Connect your wallet first.
             </h1>
             <p className="text-white text-lg">
               You need to connect your wallet to start deploying new contracts.
             </p>
-            <WalletConnector />
+            <div className="flex justify-center lg:justify-start">
+              <WalletConnector />
+            </div>
           </div>
           <div className="hidden lg:flex lg:flex-1 relative w-full h-full justify-end">
             <img
